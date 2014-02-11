5cd1d3cb2edc09c3f342f9b600383eaa402df772
Adam Hendricksen <adam@nird.us>
2014-02-10 16:51:24 -0800

    Initializing basic rails directory

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ZurbDemo::Application.config.secret_key_base = 'c51078b550174b14c7c3966ff63f5d3a3d196e0fbfba31ae901e3aef757a34174270df34476668d3639bf242fb31329c2e830955f2a8fbbe4c13d3e58caceb5c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ZurbDemo::Application.config.session_store :cookie_store, key: '_zurb-demo_session'
